fd86f13aa5b4c20d746782062d0a277f2a30e07e
DERMOT <dermotboyled@gmail.com>
2020-04-17 21:35:33 +0200

    fetching and search api data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState, useRef} from 'react';
+import axios from 'axios'
+
 
 function App() {
+
+  const [results, setResults] = useState([])
+  const [query, setQuery]= useState('react hooks')
+  const [loading, setLoading] = useState(false)
+  const [error,setError] = useState(null);
+  const searchInputRef = useRef()
+
+  useEffect(() => {
+  getResults(); 
+  }, [])
+
+
+  const handleSearch = event => {
+    event.preventDefault();
+    getResults();
+  }
+
+  const handleClearSearch = () => {
+    setQuery('');
+    searchInputRef.current.focus();
+  }
+
+  const getResults = async () => {
+    setLoading(true);
+    try{
+    const response = await axios.get
+      (`http://hn.algolia.com/api/v1/search?query=${query}`);
+     setResults(response.data.hits);
+    } catch (err) {
+      setError(err)
+    }
+     setLoading(false)
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <form onSubmit={handleSearch}>
+    <input type="text" className="query" ref={searchInputRef} value={query} onChange={event => setQuery(event.target.value)}/>
+    <button type="submit">search</button>
+    <button type="button" onClick={handleClearSearch}>Clear</button>
+    </form>
+    { !loading ?
+     <ul>
+ {results.map(result => (  
+<li key={result.objectID}>
+ <a href={result.url}>{result.title} {result.objectID}</a>
+</li>
+ ))}
+ </ul>
+: "we're getting the data y'all"}
+{error && <p>{error.message}</p>}
+    </>
   );
 }
 
